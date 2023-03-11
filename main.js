23df90a0d29eadd44f8f1db745f9662cd13a8515
Enis <enis.mema@gmail.com>
2023-03-11 13:01:52 +0100

    Made better design still background image problem

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 const pokeCards = document.querySelector('#poke-cards');
+const colors = {
+  fire: '#FDDFDF',
+  grass: '#DEFDE0',
+  electric: '#FCF7DE',
+  water: '#DEF3FD',
+  ground: '#f4e7da',
+  rock: '#d5d5d4',
+  fairy: '#fceaff',
+  poison: '#98d7a5',
+  bug: '#f8d5a3',
+  dragon: '#97b3e6',
+  psychic: '#eaeda1',
+  flying: '#F5F5F5',
+  fighting: '#E6E0D4',
+  normal: '#F5F5F5',
+};
+const main_types = Object.keys(colors);
 
 const fetchPokemon = async () => {
   for (let i = 1; i <= 25; i++) {
@@ -17,10 +34,13 @@ const CreatePokemonCard = (pokemon) => {
   pokemonEl.classList.add('card');
   const pokemonName = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
   const poke_types = pokemon.types.map((el) => el.type.name);
+  const type = main_types.find((type) => poke_types.indexOf(type) > -1);
+  const color = colors[type];
+  pokemonEl.style.backgroundColor = color;
   pokemonInnerhtml = `
   <h3>${pokemonName}</h3>
   <img src="${pokemon.sprites.front_default}" alt="" />
-  <h4 class="type">${poke_types}</h4>`;
+  <h4 class="type">Type: ${type}</h4>`;
 
   pokemonEl.innerHTML = pokemonInnerhtml;
   pokeCards.appendChild(pokemonEl);
